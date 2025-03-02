b31c91ffeaf371cc682d0ecdd22821127511320e
Ralph Largo <devlargs@gmail.com>
2025-03-02 19:22:46 +0800

    fix: export toKebabCase

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { camelToSentenceCase } from "./camelToSentenceCase";
 export { coercedGet } from "./coercedGet";
 export { isValidEmail } from "./isValidEmail";
 export { shuffleArray } from "./shuffleArray";
+export { toKebabCase } from "./toKebabCase";
